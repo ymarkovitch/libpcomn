75dac8914f1eac52ba7099bd8da4ded1879ebd3b
Yakov Markovich <ymarkovich@ptsecurity.com>
2014-09-19 23:13:23 +0400

    Start implementing keyed sets

---
+++ pcommon/pcomn_keyedset.h
@@ -0,0 +1,214 @@
+/*-*- mode:c++;tab-width:3;indent-tabs-mode:nil;c-file-style:"ellemtel";c-file-offsets:((innamespace . 0)(inclass . ++)) -*-*/
+#ifndef __PCOMN_KEYEDSET_H
+#define __PCOMN_KEYEDSET_H
+/*******************************************************************************
+ FILE         :   pcomn_keyedset.h
+ COPYRIGHT    :   Yakov Markovitch, 2014
+
+ DESCRIPTION  :
+
+ PROGRAMMED BY:   Yakov Markovitch
+ CREATION DATE:   19 Sep 2014
+*******************************************************************************/
+#include <pcomn_utils.h>
+
+#include <map>
+#include <unordered_map>
+
+namespace pcomn {
+
+template<typename Key, typename Value, typename Compare>
+using std_map = std::map<Key, Value, Compare> ;
+
+template<typename Key, typename Value, typename Compare>
+using std_multimap = std::multimap<Key, Value, Compare> ;
+
+template<typename Key, typename Value, typename Hash, typename Equal>
+using std_unordered_map = std::unordered_map<Key, Value, Hash, Equal> ;
+
+template<typename Key, typename Value, typename Hash, typename Equal>
+using std_unordered_multimap = std::unordered_multimap<Key, Value, Hash, Equal> ;
+
+/******************************************************************************/
+/**
+*******************************************************************************/
+template<typename Value, typename ExtractKey>
+struct keyval_adapter {
+      typedef ExtractKey   key_extract ;
+      typedef Value        value_type ;
+      typedef typename result_of<ExtractKey(Value)>::type    key_type ;
+      typedef typename std::remove_reference<key_type>::type keyval_type ;
+
+      template<typename Compare>
+      struct comparator : std::binary_function<keyval_adapter, keyval_adapter, bool> {
+            comparator() = default ;
+            comparator(const Compare &actual) : _compare(actual) {}
+
+            bool operator()(const keyval_adapter &x, const keyval_adapter &y) const
+            {
+               return _compare(x.key(), y.key()) ;
+            }
+
+            const key_compare &base() const { return _compare ; }
+         private:
+            Compare _compare ;
+      } ;
+
+      template<typename Hash>
+      struct hasher : std::unary_function<keyval_adapter, size_t> {
+            hasher() = default ;
+            hasher(const Hash &actual) : _hash(actual) {}
+
+            size_t operator()(const keyval_adapter &k) const { return _hash(k.key()) ; }
+         private:
+            Hash _hash ;
+      } ;
+
+      keyval_adapter(const key_type &key) { _ptr.set<0>(&key) ; }
+
+      key_type key() const
+      {
+         if (const keyval_type * const key = _ptr.get<0>())
+            return *key ;
+         return _extract_key(_ptr.get<1>()) ;
+      }
+
+   private:
+      tagged_ptr_union<const keyval_type *, const value_type *> _ptr ;
+
+      static const key_extract _extract_key ;
+
+   private:
+      PCOMN_NONCOPYABLE(keyval_adapter) ;
+      PCOMN_NONASSIGNABLE(keyval_adapter) ;
+
+      typedef std::pair<keyval_adapter<value_type, key_extract>, value_type> item_type ;
+
+   protected:
+      keyval_adapter()
+      {
+         _ptr.set<1>(reinterpret_cast<const value_type *>
+                     (reinterpret_cast<char *>(this) + (offsetof(item_type, second) - offsetof(item_type, first)))) ;
+      }
+} ;
+
+template<typename Value, typename ExtractKey>
+const keyval_adapter<Value, ExtractKey>::key_extract
+keyval_adapter<Value, ExtractKey>::_extract_key = {} ;
+
+/*******************************************************************************
+
+*******************************************************************************/
+template<typename Value, typename ExtractKey,
+         typename Compare = std::less<typename noref_result_of<ExtractKey(Value)>::type>,
+         template <typename, typename, typename> class Container = std_map>
+class keyed_ordered_set {
+      typedef keyval_adapter<Value, ExtractKey>                    base_key_type ;
+      typedef template typename base_key_type::comparator<Compare> base_key_compare ;
+      typedef Container<base_key_type, Value, base_key_compare>    base_map_type ;
+   public:
+      typedef Value value_type ;
+      typedef typename base_key_type::keyval_type key_type ;
+
+      keyed_ordered_set() = default ;
+      keyed_ordered_set(const keyed_ordered_set &) = default ;
+      keyed_ordered_set(keyed_ordered_set &&) = default ;
+
+      explicit keyed_ordered_set(const Compare &c) : _storage(base_key_compare(c)) {}
+
+      keyed_ordered_set(initializer_list<value_type> values) : _storage(values) {}
+
+      template<typename InputIterator>
+      keyed_ordered_set(InputIterator first, InputIterator last, const Compare &comp) :
+         _storage(first, last, base_key_compare(c))
+      {}
+
+      template<typename InputIterator>
+      keyed_ordered_set(InputIterator first, InputIterator last) :
+         _storage(first, last, base_key_compare())
+      {}
+
+      keyed_ordered_set &operator=(const keyed_ordered_set &) = default ;
+      keyed_ordered_set &operator=(keyed_ordered_set &&) = default ;
+
+      keyed_ordered_set &operator=(initializer_list<value_type> values)
+      {
+         keyed_ordered_set newset (values) ;
+         swap(newset) ;
+         return *this ;
+      }
+
+      key_compare key_comp() const { return storage().key_comp().base() ; }
+
+      iterator begin() const noexcept { return storage().begin() ; }
+      iterator end() const noexcept { return storage().end() ; }
+
+      reverse_iterator rbegin() const noexcept { return storage().rbegin() ; }
+      reverse_iterator rend() const noexcept { return storage().rend() ; }
+
+      iterator cbegin() const noexcept { return storage().cbegin() ; }
+      iterator cend() const noexcept { return storage().cend() ; }
+
+      reverse_iterator crbegin() const noexcept { return storage().crbegin() ; }
+      reverse_iterator crend() const noexcept { return storage().crend() ; }
+
+      bool empty() const noexcept { return storage().empty() ; }
+      size_type size() const noexcept { return storage().size() ; }
+
+      size_type max_size() const noexcept { return storage().max_size() ; }
+
+      void swap(keyed_ordered_set &other) { storage().swap(other.storage()) ; }
+
+      template<typename... Args>
+      std::pair<iterator, bool> emplace(Args&&... args) ;
+
+      template<typename... Args>
+      iterator	emplace_hint(const_iterator pos, Args &&... args) ;
+
+      std::pair<iterator, bool> insert(const value_type &x) ;
+      std::pair<iterator, bool> insert(value_type &&x) ;
+
+      iterator insert(const_iterator position, const value_type &x) ;
+      iterator insert(const_iterator position, value_type &&x) ;
+
+      template<typename InputIterator>
+      void insert(InputIterator first, InputIterator last) ;
+      void insert(initializer_list<value_type>) ;
+
+      iterator erase(const_iterator position) ;
+      size_type erase(const key_type &x) ;
+      iterator erase(const_iterator first, const_iterator last) ;
+
+      void clear() { storage().clear() ; }
+
+      size_type count(const key_type &x) const ;
+
+      iterator find(const key_type &x) ;
+      const_iterator find(const key_type &x) const ;
+
+      iterator lower_bound(const key_type &x) ;
+      const_iterator lower_bound(const key_type &x) const ;
+
+      iterator upper_bound(const key_type &x) ;
+      const_iterator upper_bound(const key_type &x) const ;
+
+      std::pair<iterator, iterator> equal_range(const key_type &x) ;
+      std::pair<const_iterator, const_iterator> equal_range(const key_type &x) const ;
+
+   private:
+      base_map_type _container ;
+
+      base_map_type &container() { return _container ; }
+      const base_map_type &container() const { return _container ; }
+} ;
+
+template<typename Value, typename ExtractKey,
+         typename Hash = std::hash<typename noref_result_of<ExtractKey(Value)>::type>,
+         typename Equal = std::equal_to<typename noref_result_of<ExtractKey(Value)>::type>,
+         template <typename, typename, typename, typename> class Container = std_unordered_map>
+class keyed_unordered_set {
+} ;
+
+} // end of pcomn namespace
+
+#endif /* __PCOMN_KEYEDSET_H */
---
+++ pcommon/unittests/unittest_keyedset.cpp
@@ -0,0 +1,200 @@
+/*-*- tab-width:3; indent-tabs-mode:nil; c-file-style:"ellemtel"; c-file-offsets:((innamespace . 0)(inclass . ++)) -*-*/
+/*******************************************************************************
+ FILE         :   unittest_keyedset.cpp
+ COPYRIGHT    :   Yakov Markovitch, 2014. All rights reserved.
+                  See LICENSE for information on usage/redistribution.
+
+ DESCRIPTION  :   Unittests for a keyed set adapters.
+
+ PROGRAMMED BY:   Yakov Markovitch
+ CREATION DATE:   19 Sep 2014
+*******************************************************************************/
+#include <pcomn_keyedset.h>
+#include <pcomn_string.h>
+#include <pcomn_unittest.h>
+
+#include <typeinfo>
+#include <memory>
+#include <fstream>
+
+#include <stdio.h>
+
+/*******************************************************************************
+                     class KeyedSetTests
+*******************************************************************************/
+class KeyedSetTests : public CppUnit::TestFixture {
+
+   private:
+      void Test_Keyed_Set_Insert() ;
+      void Test_Keyed_Set_Erase() ;
+      void Test_Keyed_Set_Extract_Key() ;
+
+      CPPUNIT_TEST_SUITE(KeyedSetTests) ;
+
+      CPPUNIT_TEST(Test_Keyed_Set_Insert) ;
+      CPPUNIT_TEST(Test_Keyed_Set_Erase) ;
+      CPPUNIT_TEST(Test_Keyed_Set_Extract_Key) ;
+
+      CPPUNIT_TEST_SUITE_END() ;
+} ;
+
+void KeyedSetTests::Test_Keyed_Set_Insert()
+{
+   test_inthashtable IntHash(4) ;
+   CPPUNIT_LOG_EQUAL(IntHash.max_load_factor(), (float)0.75) ;
+   CPPUNIT_LOG_EQUAL(IntHash.bucket_count(), (size_t)6) ;
+
+   CPPUNIT_LOG_ASSERT(IntHash.insert(10).second) ;
+   CPPUNIT_LOG(IntHash << std::endl) ;
+   CPPUNIT_LOG_EQUAL(IntHash.size(), (size_t)1) ;
+   CPPUNIT_LOG_ASSERT(IntHash.insert(4).second) ;
+   CPPUNIT_LOG_EQUAL(IntHash.size(), (size_t)2) ;
+   CPPUNIT_LOG_EQUAL(*IntHash.insert(11).first, 11L) ;
+   CPPUNIT_LOG_EQUAL(IntHash.size(), (size_t)3) ;
+   CPPUNIT_LOG(IntHash << " load_factor=" << IntHash.load_factor() << std::endl) ;
+
+   CPPUNIT_LOG_EQUAL(IntHash.count(4), (size_t)1) ;
+   CPPUNIT_LOG_EQUAL(IntHash.count(11), (size_t)1) ;
+   CPPUNIT_LOG_EQUAL(IntHash.count(10), (size_t)1) ;
+   CPPUNIT_LOG_EQUAL(IntHash.count(5), (size_t)0) ;
+
+   CPPUNIT_LOG_EQUAL(*IntHash.find(4), 4L) ;
+   CPPUNIT_LOG_EQUAL(*IntHash.find(11), 11L) ;
+   CPPUNIT_LOG_EQUAL(*IntHash.find(10), 10L) ;
+   CPPUNIT_LOG_EQUAL(IntHash.find(5), IntHash.end()) ;
+
+   CPPUNIT_LOG_ASSERT(IntHash.insert(5).second) ;
+   CPPUNIT_LOG(IntHash << std::endl) ;
+   CPPUNIT_LOG_ASSERT(IntHash.insert(26).second) ;
+   CPPUNIT_LOG(IntHash << std::endl) ;
+   CPPUNIT_LOG_ASSERT(IntHash.insert(28).second) ;
+   CPPUNIT_LOG(IntHash << std::endl) ;
+   CPPUNIT_LOG(IntHash << std::endl) ;
+
+   CPPUNIT_LOG_EQUAL(*IntHash.find(4), 4L) ;
+   CPPUNIT_LOG_EQUAL(*IntHash.find(11), 11L) ;
+   CPPUNIT_LOG_EQUAL(*IntHash.find(10), 10L) ;
+   CPPUNIT_LOG_EQUAL(*IntHash.find(5), 5L) ;
+   CPPUNIT_LOG_EQUAL(*IntHash.find(26), 26L) ;
+   CPPUNIT_LOG_EQUAL(*IntHash.find(28), 28L) ;
+   CPPUNIT_LOG_EQUAL(IntHash.find(55), IntHash.end()) ;
+}
+
+void KeyedSetTests::Test_Keyed_Set_Erase()
+{
+   test_inthashtable IntHash ;
+   CPPUNIT_LOG_EQUAL(IntHash.erase(20), (size_t)0) ;
+   CPPUNIT_LOG(IntHash << std::endl) ;
+   CPPUNIT_LOG_EQUAL(IntHash.size(), (size_t)0) ;
+   long Value = 30 ;
+   CPPUNIT_LOG_EQUAL(IntHash.erase(20, Value), (size_t)0) ;
+   CPPUNIT_LOG_EQUAL(Value, 30L) ;
+
+   CPPUNIT_LOG_ASSERT(IntHash.insert(20).second) ;
+   CPPUNIT_LOG_EQUAL(IntHash.size(), (size_t)1) ;
+   CPPUNIT_LOG_EQUAL(IntHash.erase(20), (size_t)1) ;
+   CPPUNIT_LOG_EQUAL(IntHash.size(), (size_t)0) ;
+   CPPUNIT_LOG(IntHash << std::endl) ;
+   CPPUNIT_LOG_EQUAL(IntHash.begin(), IntHash.end()) ;
+
+   CPPUNIT_LOG(std::endl) ;
+   CPPUNIT_LOG("Testing erasing in the presence of collisions" << std::endl) ;
+   const int BucketCount = IntHash.bucket_count() ;
+   CPPUNIT_LOG("Bucket count = " << BucketCount << std::endl) ;
+
+   CPPUNIT_LOG_ASSERT(IntHash.insert(BucketCount + 4).second) ;
+   CPPUNIT_LOG_ASSERT(IntHash.insert(4).second) ;
+   CPPUNIT_LOG_ASSERT(IntHash.insert(BucketCount + 5).second) ;
+   CPPUNIT_LOG_ASSERT(IntHash.insert(5).second) ;
+   CPPUNIT_LOG_EQUAL(IntHash.size(), (size_t)4) ;
+   CPPUNIT_LOG(IntHash << std::endl) ;
+   CPPUNIT_LOG_EQUAL(IntHash.erase(BucketCount + 4), (size_t)1) ;
+   CPPUNIT_LOG_EQUAL(IntHash.size(), (size_t)3) ;
+   CPPUNIT_LOG(IntHash << std::endl) ;
+   CPPUNIT_LOG_EQUAL(IntHash.count(BucketCount + 4), (size_t)0) ;
+   CPPUNIT_LOG_EQUAL(IntHash.count(4), (size_t)1) ;
+   CPPUNIT_LOG_EQUAL(*IntHash.find(4), 4L) ;
+   CPPUNIT_LOG_EQUAL(*IntHash.find(5), 5L) ;
+   CPPUNIT_LOG_EQUAL(*IntHash.find(BucketCount + 5), BucketCount + 5L) ;
+   CPPUNIT_LOG_EQUAL(IntHash.erase(4), (size_t)1) ;
+   CPPUNIT_LOG_EQUAL(IntHash.size(), (size_t)2) ;
+   CPPUNIT_LOG_EQUAL(IntHash.count(4), (size_t)0) ;
+   CPPUNIT_LOG_EQUAL(*IntHash.find(5), 5L) ;
+   CPPUNIT_LOG_EQUAL(*IntHash.find(BucketCount + 5), BucketCount + 5L) ;
+   CPPUNIT_LOG_EQUAL(std::distance(IntHash.begin(), IntHash.end()), (ptrdiff_t)2) ;
+
+   CPPUNIT_LOG_EQUAL(CPPUNIT_SORTED(std::vector<int>(IntHash.begin(), IntHash.end())),
+                     CPPUNIT_CONTAINER(std::vector<int>, (5)(BucketCount + 5))) ;
+
+   CPPUNIT_LOG_RUN(IntHash.erase(IntHash.find(BucketCount + 5))) ;
+
+   CPPUNIT_LOG_EQUAL(CPPUNIT_SORTED(std::vector<int>(IntHash.begin(), IntHash.end())),
+                     CPPUNIT_CONTAINER(std::vector<int>, (5))) ;
+}
+
+static const char * const Hello = "Hello" ;
+static const char * const World = "world" ;
+static const char * const Foo = "Foo" ;
+static const char * const Bar = "Bar" ;
+static const char * const Quux = "Quux" ;
+static const char * const Xyzzy = "Xyzzy" ;
+static const char * const Baby = "Baby" ;
+static const char * const Bye = "Bye" ;
+
+struct KeyedHashval {
+      uint64_t key ;
+      char     str[64] ;
+} ;
+
+struct extract_key : std::unary_function<KeyedHashval, uint64_t> {
+      uint64_t operator()(const KeyedHashval &val) const { return val.key ; }
+} ;
+
+void KeyedSetTests::Test_Keyed_Set_Extract_Key()
+{
+   typedef pcomn::closed_hashtable<KeyedHashval, extract_key> testtable_type ;
+   CPPUNIT_LOG_EQUAL(typeid(testtable_type::key_type), typeid(uint64_t)) ;
+
+   testtable_type TestHash ;
+
+   KeyedHashval values[] = {
+      { 3467, "v:3467" },
+      { 7777, "v:7777" },
+      { 7133, "v:7133" },
+      { 0, "v:0" }
+   } ;
+
+   CPPUNIT_LOG_RUN(TestHash.insert(values + 0, values + 4)) ;
+
+   CPPUNIT_LOG(TestHash << std::endl) ;
+   CPPUNIT_LOG_EQUAL(TestHash.size(), (size_t)4) ;
+
+   CPPUNIT_LOG_EQUAL(TestHash.count(3467), (size_t)1) ;
+   CPPUNIT_LOG_EQUAL(TestHash.count(7777), (size_t)1) ;
+   CPPUNIT_LOG_EQUAL(TestHash.count(7133), (size_t)1) ;
+   CPPUNIT_LOG_EQUAL(TestHash.count(0), (size_t)1) ;
+   CPPUNIT_LOG_EQUAL(TestHash.count(1), (size_t)0) ;
+
+   CPPUNIT_LOG_EQUAL(std::string(TestHash.find(3467)->str), std::string("v:3467")) ;
+   CPPUNIT_LOG_EQUAL(std::string(TestHash.find(7777)->str), std::string("v:7777")) ;
+   CPPUNIT_LOG_EQUAL(std::string(TestHash.find(7133)->str), std::string("v:7133")) ;
+   CPPUNIT_LOG_EQUAL(std::string(TestHash.find(0)->str), std::string("v:0")) ;
+
+   CPPUNIT_LOG_ASSERT(TestHash.erase(7777)) ;
+   CPPUNIT_LOG_ASSERT(TestHash.erase(7133)) ;
+   CPPUNIT_LOG_EQUAL(TestHash.count(3467), (size_t)1) ;
+   CPPUNIT_LOG_EQUAL(TestHash.count(7777), (size_t)0) ;
+   CPPUNIT_LOG_EQUAL(TestHash.count(7133), (size_t)0) ;
+   CPPUNIT_LOG_EQUAL(TestHash.count(0), (size_t)1) ;
+   CPPUNIT_LOG_EQUAL(std::string(TestHash.find(3467)->str), std::string("v:3467")) ;
+   CPPUNIT_LOG_EQUAL(std::string(TestHash.find(0)->str), std::string("v:0")) ;
+}
+
+int main(int argc, char *argv[])
+{
+   pcomn::unit::TestRunner runner ;
+   runner.addTest(KeyedSetTests::suite()) ;
+
+   return
+      pcomn::unit::run_tests(runner, argc, argv, "unittest.diag.ini", "Keyed set adapters tests") ;
+}
