aa7f5c2d33a9a421ca0720e3ee83116020a4fbf0
Yakov Markovitch <ymarkovitch@gmail.com>
2016-06-23 18:37:28 +0300

    Fixed dummy node handling in M&S queue   - do not allocate dummy node  in the heap   - unconditionally delete all remaining non-dummy nodes in destructor

--- pcommon/pcomn_cdsqueue.h
+++ pcommon/pcomn_cdsqueue.h
@@ -19,15 +19,24 @@ namespace pcomn {
 namespace detail {
 
 template<typename T>
-struct dynqueue_node {
-      PCOMN_NONCOPYABLE(dynqueue_node) ;
-      PCOMN_NONASSIGNABLE(dynqueue_node) ;
+struct dynqueue_node ;
 
+template<typename T>
+struct dynqueue_node_nextptr {
+      PCOMN_NONCOPYABLE(dynqueue_node_nextptr) ;
+      PCOMN_NONASSIGNABLE(dynqueue_node_nextptr) ;
+
+      constexpr dynqueue_node_nextptr(dynqueue_node<T> *next = nullptr) : _next(next) {}
+
+      dynqueue_node<T> *_next ;
+} ;
+
+template<typename T>
+struct dynqueue_node : dynqueue_node_nextptr<T> {
       template<typename... Args>
       dynqueue_node(Args &&...args) : _value(std::forward<Args>(args)...) {}
 
-      dynqueue_node *_next ;
-      T              _value ;
+      T _value ;
 } ;
 }
 
@@ -62,6 +71,14 @@ class concurrent_dynqueue :
 
       concurrent_dynqueue() = default ;
 
+      /// The destructor pops and @em immediately destroys and deallocates all nodes
+      /// remaining in the queue.
+      ///
+      /// No hazard pointers and safe memory reclamation here: it is assumed a queue is
+      /// destructed in exclusive mode, it is illegal to concurrently access the queue
+      /// being destroyed.
+      ~concurrent_dynqueue() ;
+
       void push(const value_type &value)
       {
          emplace(value) ;
@@ -77,15 +94,23 @@ class concurrent_dynqueue :
          push_node(make_node(std::forward<Args>(args)...)) ;
       }
 
+      /// Alias for push() to allow for std::back_insert_iterator
+      void push_back(const value_type &value) { push(value) ; }
+      /// @overload
+      void push_back(value_type &&value) { push(std::move(value)) ; }
+
       bool pop(value_type &result) ;
+
       template<typename... Args>
       std::pair<value_type, bool> pop_default(Args &&...defargs) ;
 
       bool empty() const { return _head == _tail ; }
 
    private:
-      node_type *_head = nullptr ;
-      node_type *_tail = nullptr ;
+      detail::dynqueue_node_nextptr<T> _dummy_node = {nullptr} ;
+
+      node_type *_head = static_cast<node_type *>(&_dummy_node) ;
+      node_type *_tail = static_cast<node_type *>(&_dummy_node) ;
 
    private:
       // Allocate and construct a queue node
@@ -99,29 +124,50 @@ class concurrent_dynqueue :
          return p.release() ;
       }
 
-      void destroy_popped_node(node_type *popped_node)
+      void retire_node(node_type *node)
       {
-         NOXCHECK(popped_node) ;
-         node_allocator_traits::destroy(this->node_allocator(), popped_node) ;
-         this->retire_node(popped_node) ;
+         if (node != &_dummy_node)
+            ancestor::retire_node(node) ;
       }
 
       void push_node(node_type *new_node) noexcept ;
-      node_type *pop_node() ;
+      node_hazard_ptr pop_node() ;
 } ;
 
 /*******************************************************************************
  concurrent_dynqueue
 *******************************************************************************/
 template<typename T, typename A>
+concurrent_dynqueue<T, A>::~concurrent_dynqueue()
+{
+   if (_head == &_dummy_node)
+   {
+      NOXCHECK(_tail == _head) ;
+      return ;
+   }
+
+   for (;;)
+   {
+      node_type *node = _head ;
+      _head = _head->_next ;
+      node_allocator_traits::deallocate(this->node_allocator(), node, 1) ;
+      if (!_head)
+         return ;
+      node_allocator_traits::destroy(this->node_allocator(), _head) ;
+   }
+}
+
+template<typename T, typename A>
 bool concurrent_dynqueue<T, A>::pop(value_type &result)
 {
-   node_type *popped_head = pop_node() ;
+   node_hazard_ptr popped_head = pop_node() ;
    if (!popped_head)
       return false ;
 
    result = std::move(popped_head->_value) ;
-   destroy_popped_node(popped_head) ;
+   // Call the destructor but do _not_ retire/deallocate:
+   // this node (maybe) becomes a dummy node.
+   node_allocator_traits::destroy(this->node_allocator(), popped_head.get()) ;
 
    return true ;
 }
@@ -130,7 +176,7 @@ template<typename T, typename A>
 template<typename... Args>
 auto concurrent_dynqueue<T, A>::pop_default(Args &&...defargs) -> std::pair<value_type, bool>
 {
-   node_type *popped_head = pop_node() ;
+   node_hazard_ptr popped_head = pop_node() ;
    if (!popped_head)
       return
       {std::piecewise_construct,
@@ -138,7 +184,7 @@ auto concurrent_dynqueue<T, A>::pop_default(Args &&...defargs) -> std::pair<valu
             std::forward_as_tuple(false)} ;
 
    std::pair<value_type, bool> result {std::move(popped_head->_value), true} ;
-   destroy_popped_node(popped_head) ;
+   node_allocator_traits::destroy(this->node_allocator(), popped_head.get()) ;
 
    return std::move(result) ;
 }
@@ -147,7 +193,7 @@ template<typename T, typename A>
 void concurrent_dynqueue<T, A>::push_node(node_type *new_node) noexcept
 {
    NOXCHECK(new_node) ;
-   NOXCHECK(!new_node->next) ;
+   NOXCHECK(!new_node->_next) ;
 
    // Hold the pushed node safe until enqueue is finished
    const node_hazard_ptr node (new_node) ;
@@ -157,11 +203,11 @@ void concurrent_dynqueue<T, A>::push_node(node_type *new_node) noexcept
    for(;;)
    {
       const node_hazard_ptr tail (&_tail) ;
-      old_tail = tail ;
+      old_tail = tail.get() ;
 
       // When a tail is still the tail and the queue is not in the middle of enqueuing of
       // a node by someone else, tail->_next shall be NULL.
-      if (const node_hazard_ptr tail_next = tail->_next)
+      if (const node_hazard_ptr tail_next {tail->_next})
       {
          // Tail is not pointing to the last node, help someone else who is now
          // in the middle of enqueuing to swing the tail.
@@ -180,15 +226,17 @@ void concurrent_dynqueue<T, A>::push_node(node_type *new_node) noexcept
 }
 
 template<typename T, typename A>
-inline auto concurrent_dynqueue<T, A>::pop_node() -> node_type *
+inline auto concurrent_dynqueue<T, A>::pop_node() -> node_hazard_ptr
 {
    // Keep trying until successful dequeue
    for(;;)
    {
-      const node_hazard_ptr head (&_head) ;
-      const node_hazard_ptr next (head->_next) ;
+      node_hazard_ptr head (&_head) ;
+      node_hazard_ptr next (head->_next) ;
 
-      if (head == _tail)
+      node_type *head_ptr = head.get() ;
+
+      if (head_ptr == _tail)
       {
          if (!next)
             // The queue is empty
@@ -196,12 +244,16 @@ inline auto concurrent_dynqueue<T, A>::pop_node() -> node_type *
 
          // The queue is in the middle of enqueuing of the first node, tail is falling
          // behind, help to advance it.
-         atomic_op::cas(&_tail, head.get(), next.get(), std::memory_order_release) ;
+         atomic_op::cas(&_tail, head_ptr, next.get(), std::memory_order_release) ;
          continue ;
       }
 
-      if (atomic_op::cas(&_head, head.get(), next.get(), std::memory_order_release))
-         return head.get() ;
+      if (atomic_op::cas(&_head, head_ptr, next.get(), std::memory_order_release))
+      {
+         head.reset() ;
+         retire_node(head_ptr) ;
+         return std::move(next) ;
+      }
    }
 }
 
--- pcommon/unittests/unittest_cdsqueue.cpp
+++ pcommon/unittests/unittest_cdsqueue.cpp
@@ -30,15 +30,60 @@ class ConcurrentDynQueueTests : public CppUnit::TestFixture {
 *******************************************************************************/
 void ConcurrentDynQueueTests::Test_CdsQueue_SingleThread()
 {
-   typedef concurrent_dynqueue<std::string> cdsq_string ;
+   typedef concurrent_dynqueue<std::string> strcdsq ;
 
-   cdsq_string empty0 ;
+   strcdsq empty0 ;
    std::string val0 = "Hello, world!" ;
 
    CPPUNIT_LOG_ASSERT(empty0.empty()) ;
    CPPUNIT_LOG_IS_FALSE(empty0.pop(val0)) ;
    CPPUNIT_LOG_ASSERT(empty0.empty()) ;
    CPPUNIT_LOG_EQ(val0, "Hello, world!") ;
+   CPPUNIT_LOG_EQ(empty0.pop_default("Foo"), std::make_pair("Foo", false)) ;
+   CPPUNIT_LOG_ASSERT(empty0.empty()) ;
+
+   CPPUNIT_LOG(std::endl) ;
+
+   strcdsq q1 ;
+   std::string val1 = "Bye, baby!" ;
+   std::string val ;
+
+   CPPUNIT_LOG_ASSERT(q1.empty()) ;
+   CPPUNIT_LOG_IS_FALSE(q1.pop(val1)) ;
+   CPPUNIT_LOG_EQ(val1, "Bye, baby!") ;
+   CPPUNIT_LOG_RUN(q1.push(val0)) ;
+   CPPUNIT_LOG_IS_FALSE(q1.empty()) ;
+
+   CPPUNIT_LOG_ASSERT(q1.pop(val)) ;
+   CPPUNIT_LOG_EQ(val, "Hello, world!") ;
+   CPPUNIT_LOG_ASSERT(q1.empty()) ;
+   CPPUNIT_LOG_EQ(q1.pop_default("Foo"), std::make_pair("Foo", false)) ;
+   CPPUNIT_LOG_ASSERT(q1.empty()) ;
+
+   CPPUNIT_LOG(std::endl) ;
+   CPPUNIT_LOG_RUN(q1.push("Foo")) ;
+   CPPUNIT_LOG_IS_FALSE(q1.empty()) ;
+   CPPUNIT_LOG_RUN(q1.push_back("Quux")) ;
+   CPPUNIT_LOG_IS_FALSE(q1.empty()) ;
+   CPPUNIT_LOG_RUN(q1.push(std::move(val0))) ;
+   CPPUNIT_LOG_IS_FALSE(q1.empty()) ;
+   CPPUNIT_LOG_EQ(val0, "") ;
+   CPPUNIT_LOG_RUN(q1.emplace(16, '@')) ;
+   CPPUNIT_LOG_IS_FALSE(q1.empty()) ;
+
+   CPPUNIT_LOG(std::endl) ;
+   CPPUNIT_LOG_ASSERT(q1.pop(val)) ;
+   CPPUNIT_LOG_EQ(val, "Foo") ;
+   CPPUNIT_LOG_IS_FALSE(q1.empty()) ;
+   CPPUNIT_LOG_EQ(q1.pop_default(), std::make_pair("Quux", true)) ;
+   CPPUNIT_LOG_IS_FALSE(q1.empty()) ;
+   CPPUNIT_LOG_EQ(q1.pop_default(), std::make_pair("Hello, world!", true)) ;
+   CPPUNIT_LOG_IS_FALSE(q1.empty()) ;
+   CPPUNIT_LOG_EQ(q1.pop_default(), std::make_pair("@@@@@@@@@@@@@@@@", true)) ;
+   CPPUNIT_LOG_ASSERT(q1.empty()) ;
+   CPPUNIT_LOG_EQ(q1.pop_default(), std::make_pair("", false)) ;
+
+   CPPUNIT_LOG(std::endl) ;
 }
 
 /*******************************************************************************
