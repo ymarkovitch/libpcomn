5d44251b216bcb861122fcc49786b325322d667b
Yakov Markovitch <ymarkovitch@gmail.com>
2017-09-18 18:46:21 +0300

    Add Leo Yuriev to CREDITS.txt

---
+++ pcommon/pcomn_ssafe.cpp
@@ -0,0 +1,10 @@
+/*-*- tab-width:4;indent-tabs-mode:nil;c-file-style:"ellemtel";c-basic-offset:4;c-file-offsets:((innamespace . 0)(inlambda . 0)) -*-*/
+/*******************************************************************************
+ FILE         :   pcomn_ssafe.cpp
+ COPYRIGHT    :   Yakov Markovitch, 2017
+
+ DESCRIPTION  :
+
+ PROGRAMMED BY:   Yakov Markovitch
+ CREATION DATE:   18 Sep 2017
+*******************************************************************************/
