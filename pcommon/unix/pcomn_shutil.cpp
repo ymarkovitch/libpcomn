f29852ec0e46872e565430300c6ba558bfc375f0
Yakov Markovitch <ymarkovitch@gmail.com>
2018-08-13 20:20:07 +0300

    Landed pcomn::sys::rm

--- pcommon/unix/pcomn_shutil.cpp
+++ pcommon/unix/pcomn_shutil.cpp
@@ -99,6 +99,86 @@ bool copytree(const pcomn::strslice &sourcedir, const pcomn::strslice &destdir, 
 *******************************************************************************/
 typedef std::function<void(int, const char *, const fsstat &)> skip_logger ;
 
+namespace {
+struct rm_handler {
+      rm_handler(RmFlags flags, const skip_logger &logger) :
+         _flags(flags), _skiplogger(logger)
+      {}
+
+      const rm_info &info() const { return _info ; }
+      rm_info &info() { return _info ; }
+      const std::exception_ptr &last_exception() { return _exception ; }
+
+      int rmfile(const char *fpath, const fsstat &s, int typeflag)
+      {
+         int lasterr = 0 ;
+
+         auto unlink_file = [this](const char *fpath, const fsstat &s)
+         {
+            const int err = posix_errno(unlink(fpath)) ;
+            _info._rm_size += s.st_size * !err * !!S_ISREG(s.st_mode) ;
+            return err ;
+         } ;
+
+         ++_info._visit_count ;
+
+         if (typeflag == FTW_DP)
+         {
+            // directory, after processing contents
+            lasterr = posix_errno(rmdir(fpath)) ;
+            if (lasterr == ENOTDIR)
+               lasterr = unlink_file(fpath, filestat(fpath)) ;
+         }
+         else
+         {
+            lasterr = unlink_file(fpath, s) ;
+            if (lasterr == EISDIR)
+               lasterr = posix_errno(rmdir(fpath)) ;
+         }
+
+         if (!lasterr)
+            return 0 ;
+
+         if (lasterr == ENOENT && (_flags & RM_IGNORE_NEXIST))
+            --_info._visit_count ;
+         else
+         {
+            ++_info._skip_count ;
+            append_exception(lasterr, fpath, s) ;
+         }
+         return 0 ;
+      }
+
+   private:
+      RmFlags     _flags ;
+      skip_logger _skiplogger ;
+      rm_info     _info ;
+      std::string _xinfo ;
+      std::exception_ptr _exception ;
+
+   private:
+      void append_exception(int err, const char *fpath, const fsstat &s) noexcept
+      {
+         if (_skiplogger)
+            try { _skiplogger(err, fpath, s) ; } catch (...) {} ;
+
+         if (_flags & RM_IGNORE_ERRORS)
+            return ;
+
+         try {
+            PCOMN_CHECK_POSIX((errno = err) ? -1 : 0,
+                              _exception ? "%s\n%s '%s'" : "%s%s '%s'",
+                              _xinfo.c_str(), "Error while attempting to remove", fpath) ;
+         }
+         catch (std::exception &x)
+         {
+            _exception = std::current_exception() ;
+            _xinfo = x.what() ;
+         }
+      }
+} ;
+} // end of unnamed namespace
+
 rm_info rm(const pcomn::strslice &path, const skip_logger &skiplogger, RmFlags flags)
 {
    PCOMN_ENSURE_ARG(path) ;
@@ -126,64 +206,10 @@ rm_info rm(const pcomn::strslice &path, const skip_logger &skiplogger, RmFlags f
    /****************************************************************************
     Walk along the directory tree
    ****************************************************************************/
-   struct file_handler {
-         RmFlags     _flags ;
-         skip_logger _skiplogger ;
-         rm_info     _info ;
-         std::string _xinfo ;
-         std::exception_ptr _exception ;
-   } handler = { flags, skiplogger ? skiplogger : [](int, const char *, const fsstat &){} } ;
+   rm_handler handler (flags, skiplogger) ;
 
-   static thread_local file_handler *current_handler = nullptr ;
-   const vsaver<file_handler *> set_handler (current_handler, &handler) ;
-
-   /****************************************************************************
-    Main handler, passed to nftw
-   ****************************************************************************/
-   auto rm_file = [](const char *fpath, const struct stat *sb, int typeflag, FTW *) -> int
-   {
-      int lasterr = 0 ;
-
-      auto append_exception = [](int err, const char *fpath, const fsstat &s)
-      {
-      } ;
-
-      auto unlink_file = [](const char *fpath, const fsstat &s)
-      {
-         const int err = posix_errno(unlink(fpath)) ;
-         current_handler->_info._rm_size += s.st_size * !err * !!S_ISREG(s.st_mode) ;
-         return err ;
-      } ;
-
-      ++current_handler->_info._visit_count ;
-
-      if (typeflag == FTW_DP)
-      {
-         // directory, after processing contents
-         lasterr = posix_errno(rmdir(fpath)) ;
-         if (lasterr == ENOTDIR)
-            lasterr = unlink_file(fpath, filestat(fpath)) ;
-      }
-      else
-      {
-         lasterr = unlink_file(fpath, *sb) ;
-         if (lasterr == EISDIR)
-            lasterr = posix_errno(rmdir(fpath)) ;
-      }
-
-      if (!lasterr)
-         return 0 ;
-
-      if (lasterr == ENOENT && (current_handler->_flags & RM_IGNORE_NEXIST))
-         --current_handler->_info._visit_count ;
-      else
-      {
-         ++current_handler->_info._skip_count ;
-         append_exception(lasterr, fpath, *sb) ;
-         current_handler->_skiplogger(lasterr, fpath, *sb) ;
-      }
-      return 0 ;
-   } ;
+   static thread_local rm_handler *current_handler = nullptr ;
+   const vsaver<rm_handler *> set_handler (current_handler, &handler) ;
 
    const fsstat &topstat = filestat(spath) ;
 
@@ -195,13 +221,20 @@ rm_info rm(const pcomn::strslice &path, const skip_logger &skiplogger, RmFlags f
    const int lasterr = typeflag == FTW_NS
       ? errno
       : posix_errno(typeflag == FTW_DP && (flags & RM_RECURSIVE)
-                    // The specified path is a directory: walk through the directory tree
-                    ? nftw(spath.c_str(), rm_file, 128, FTW_DEPTH | FTW_PHYS)
-                    // The specified path is a file: remove it immediatrely
-                    : rm_file(spath.c_str(), &topstat, typeflag, nullptr)) ;
 
-   if (handler._exception)
-      std::rethrow_exception(handler._exception) ;
+                    // The specified path is a directory: walk through the directory tree
+                    ? nftw(spath.c_str(),
+                           [](const char *fpath, const struct stat *sb, int typeflag, FTW *)
+                           {
+                              return current_handler->rmfile(fpath, *sb, typeflag) ;
+                           },
+                           128, FTW_DEPTH | FTW_PHYS)
+
+                    // The specified path is a file: remove it immediatrely
+                    : handler.rmfile(spath.c_str(), topstat, typeflag)) ;
+
+   if (handler.last_exception())
+      std::rethrow_exception(handler.last_exception()) ;
 
    switch (lasterr)
    {
@@ -209,11 +242,11 @@ rm_info rm(const pcomn::strslice &path, const skip_logger &skiplogger, RmFlags f
       case ENOENT: if (flags & RM_IGNORE_NEXIST) break ;
 
       default:
-         handler._info._skip_count = std::max(handler._info._skip_count, 1U) ;
+         handler.info()._skip_count = std::max(handler.info()._skip_count, 1U) ;
          PCOMN_CHECK_POSIX(raise_error ? -1 : 0, "Error while removing '%s'", spath.c_str()) ;
    }
 
-   return handler._info ;
+   return handler.info() ;
 }
 
 } // end of namespace pcomn::sys
