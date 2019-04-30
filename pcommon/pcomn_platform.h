f69efe9b126dcfe84914cfd041616345ee978ed1
Yakov Markovitch <ymarkovitch@gmail.com>
2019-04-30 20:31:50 +0300

    Fix likely()/unlikely()

--- pcommon/pcomn_platform.h
+++ pcommon/pcomn_platform.h
@@ -630,8 +630,8 @@
 *******************************************************************************/
 #if !defined(likely) && !defined(unlikely)
 #ifdef PCOMN_COMPILER_GNU
-#define likely(expr)    __builtin_expect((expr), 1)
-#define unlikely(expr)  __builtin_expect((expr), 0)
+#define likely(expr)    __builtin_expect(!!(expr), 1)
+#define unlikely(expr)  __builtin_expect(!!(expr), 0)
 #else
 #define likely(expr)    (expr)
 #define unlikely(expr)  (expr)
