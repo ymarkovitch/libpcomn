98d8cc39e5361124b34b0b7b4156b1f4f59701f6
Yakov Markovitch <ymarkovitch@gmail.com>
2019-06-10 18:03:39 +0300

    Add netaddr.h as backward-compatibility forwarding header

---
+++ pcomn_net/netaddr.h
@@ -0,0 +1,11 @@
+/*-*- mode:c++;tab-width:4;indent-tabs-mode:nil;c-file-style:"ellemtel";c-basic-offset:4;c-file-offsets:((innamespace . 0)(inlambda . 0)) -*-*/
+/*******************************************************************************
+ FILE         :   netaddr.h
+ COPYRIGHT    :   Yakov Markovitch, 2019
+ PROGRAMMED BY:   Yakov Markovitch
+ CREATION DATE:   10 Jun 2019
+*******************************************************************************/
+/** @file
+ Backward compatibility header, forwards to pcomn_netaddr.h
+*******************************************************************************/
+#include <pcomn_netaddr.h>
