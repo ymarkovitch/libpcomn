7c29ca6dcbf6eca79531b72b8d209c88e144e53f
Yakov Markovitch <yakov.spare@gmail.com>
2020-03-10 15:30:39 +0300

    Introduce job_batch

--- pcommon/pcomn_threadpool.h
+++ pcommon/pcomn_threadpool.h
@@ -24,6 +24,90 @@
 namespace pcomn {
 
 /***************************************************************************//**
+ Fixed-size thread pool for one-time execution of a batch of jobs.
+
+ Unlike the usual thread pool, the object of this class does not allow adding
+ jobs after the start of processing. The workflow is as follows:
+
+  1. Create job_batch object with a specified number of threads.
+  2. Form a batch of jobs, submitting _all_ tasks to be performed.
+  3. Start processing either by calling either start() or wait() function.
+  4. After start() or wait() no new jobs can be submitted, i.e. job_batch is "one-off".
+
+ A job can be submitted with add_task() or add_job(). The difference is add_task()
+ returns std::future<> object, thus allowing to obtain the result of execution,
+ while add_job() returns void ("fire and forget").
+ ******************************************************************************/
+class job_batch {
+    PCOMN_NONCOPYABLE(job_batch) ;
+    PCOMN_NONASSIGNABLE(job_batch) ;
+public:
+    job_batch() ;
+    explicit job_batch(unsigned threadcount) ;
+
+    /// Wait until all the pending tasks from the queue have been completed.
+    ~job_batch() ;
+
+    /// Get the number of threads in the pool.
+    size_t size() const ;
+
+    /// Drop all the pending tasks from the task queue.
+    ///
+    unsigned clear_queue() ;
+
+    /// Immediately stop the pool, drop all the pending jobs.
+    /// All threads are joined and deleted.
+    ///
+    void stop() ;
+
+    /// Append the callable object to the batch and get std::future<> to allow obtaining
+    /// the result of execution.
+    ///
+    template<typename F>
+    auto add_task(F &&task) ->std::future<decltype(task())>
+    {
+        typedef decltype(task()) task_result ;
+
+        auto ptask = std::packaged_task<task_result()>(std::forward<F>(task)) ;
+        std::future<task_result> result (ptask.get_future()) ;
+
+        _task_queue.push(std::move(ptask)) ;
+
+        return result ;
+    }
+
+    /// Append the callable object to the batch.
+    ///
+    template<typename F>
+    auto add_job(F &&job) ->std::void_t<decltype(job())>
+    {
+        _task_queue.push(packed_job(std::packaged_task<void()>(std::forward<F>(job)))) ;
+    }
+
+private:
+    class alignas(cacheline_t) packed_job final : std::function<void()> {
+        typedef std::function<void()> ancestor ;
+    public:
+        using ancestor::ancestor ;
+    } ;
+
+private:
+    std::atomic<unsigned> _idle_count {0} ;  /* Count of idle threads. */
+    atomic_flag           _retire {false} ;
+    atomic_flag           _stop   {false} ;
+
+    mutable std::mutex _pool_mutex ;
+
+    std::vector<packed_job>
+    blocking_ring_queue<packed_job> _task_queue {4096} ;
+    std::list<std::pair<pthread, atomic_flag_ptr>> _threads ;
+
+private:
+    void start_thread() ;
+    void flush_task_queue() ;
+} ;
+
+/***************************************************************************//**
  A resizable thread pool to run any callable objects with signature `ret func()`.
 
  The resulting task value or exception is available through std::future<ret>.
@@ -158,6 +242,7 @@ private:
     void start_thread() ;
     void flush_task_queue() ;
 } ;
-}
+
+} // end of namespace pcomn
 
 #endif /* __PCOMN_THREADPOOL_H */
