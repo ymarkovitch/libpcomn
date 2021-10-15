1f2b13a2b858648c13abb40d3a1adbd5aa6dcf0d
Yakov Markovitch <ymarkovitch@gmail.com>
2021-10-15 23:59:35 +0300

    Allow to PCOMN_DEFINE_FLAG_ENUM as a friend

--- pcommon/pcommon.h
+++ pcommon/pcommon.h
@@ -194,30 +194,30 @@ do {                                                        \
    __VA_ARGS__ inline auto pend(const type &x) -> decltype(&*std::end(x)) { return std::end(x) ; }
 
 /// Define bit flag operations (|,&,~) over the specified enum type.
-#define PCOMN_DEFINE_FLAG_ENUM(enum_type)                               \
+#define PCOMN_DEFINE_FLAG_ENUM(enum_type, ...)                          \
    PCOMN_STATIC_CHECK(std::is_enum<enum_type>()) ;                      \
-   constexpr inline enum_type operator&(enum_type x, enum_type y)       \
+   __VA_ARGS__ constexpr inline enum_type operator&(enum_type x, enum_type y) \
    {                                                                    \
       typedef std::underlying_type_t<enum_type> int_type ;              \
       return (enum_type)((int_type)x & (int_type)y) ;                   \
    }                                                                    \
-   constexpr inline enum_type operator|(enum_type x, enum_type y)       \
+   __VA_ARGS__ constexpr inline enum_type operator|(enum_type x, enum_type y) \
    {                                                                    \
       typedef std::underlying_type_t<enum_type> int_type ;              \
       return (enum_type)((int_type)x | (int_type)y) ;                   \
    }                                                                    \
-   constexpr inline enum_type operator^(enum_type x, enum_type y)       \
+   __VA_ARGS__ constexpr inline enum_type operator^(enum_type x, enum_type y) \
    {                                                                    \
       typedef std::underlying_type_t<enum_type> int_type ;              \
       return (enum_type)((int_type)x ^ (int_type)y) ;                   \
    }                                                                    \
-   constexpr inline enum_type operator~(enum_type x)                    \
+   __VA_ARGS__ constexpr inline enum_type operator~(enum_type x)        \
    {                                                                    \
       return (enum_type)(~(std::underlying_type_t<enum_type>)x) ;       \
    }                                                                    \
-   inline enum_type &operator|=(enum_type &x, enum_type y) { return x = x | y ; } \
-   inline enum_type &operator&=(enum_type &x, enum_type y) { return x = x & y ; } \
-   inline enum_type &operator^=(enum_type &x, enum_type y) { return x = x ^ y ; }
+   __VA_ARGS__ inline enum_type &operator|=(enum_type &x, enum_type y) { return x = x | y ; } \
+   __VA_ARGS__ inline enum_type &operator&=(enum_type &x, enum_type y) { return x = x & y ; } \
+   __VA_ARGS__ inline enum_type &operator^=(enum_type &x, enum_type y) { return x = x ^ y ; }
 
 /*******************************************************************************
 *******************************************************************************/
